e8b2980aab13ae251c8f38a819c84bd513d0aa45
Geoffroy Couprie <geo.couprie@gmail.com>
2013-03-06 10:21:04 +0100

    implement js search

--- js/search.js
+++ js/search.js
@@ -1,18 +1,48 @@
-$.ajax({
-  type: 'GET',
-  url: '/search.json',
-  // type of data we are expecting in return:
-  dataType: 'json',
-  timeout: 300,
-  context: $('#results'),
-  success: function(data){
-    // Supposing this JSON payload was received:
-    //   {"project": {"id": 42, "html": "<div>..." }}
-    // append the HTML to context object.
-    console.log(data)
-    this.append(data)
-  },
-  error: function(xhr, type){
-    console.log('Ajax error!')
+window.onload = function() {
+  var input = $('#search')
+  var submit = $('#submit')
+  var urls = []
+
+  $.ajax({
+    type: 'GET',
+    url: '/urls.json',
+    dataType: 'json',
+    timeout: 2000,
+    success: function(data){
+      console.log(data)
+      urls = data
+    },
+    error: function(xhr, type){
+      console.log('Ajax error!')
+    }
+  })
+
+  function search() {
+    $('#results').text('')
+    console.log(input.val())
+    $.ajax({
+      type: 'GET',
+      url: '/search.json',
+      dataType: 'json',
+      timeout: 2000,
+      success: function(data){
+        console.log(data[input.val()])
+        var indexes = data[input.val()]
+        var results = $('#results')
+        $.map(indexes, function(item, index) {
+          var url = urls[item]
+          results.append("<li><a href=\""+url+"\">"+url+"</a></li>")
+        })
+      },
+      error: function(xhr, type){
+        console.log('Ajax error!')
+      }
+    })
   }
-})
+  submit.on("click", search)
+  input.on("keypress", function(e){
+    if(e && e.keyCode == 13) {
+      search()
+    }
+  })
+}
